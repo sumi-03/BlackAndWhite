f61ffffa23e2a1ec6347c9008fc9ef40223ac6f2
ghkdxo9565 <57709863+ghkdxo9565@users.noreply.github.com>
2024-12-03 23:26:56 +0900

    GameFrame 수정

--- src/Game/GameFrame.java
+++ src/Game/GameFrame.java
@@ -5,28 +5,31 @@ import javax.swing.*;
 public class GameFrame extends JFrame {
 
     public GameFrame() {
-
+        setTitle("BnW");
         setSize(960, 600);
-        setContentPane(new StartPanel()); // StartPanel 의 객체를 생성하여 컨텐트팬으로 설정
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        setLocationRelativeTo(null);
         setResizable(false);
+
+        // WAV 파일 목록
+        String[] wavFiles = {
+                "EvenFloor.wav",
+                "Heyday.wav",
+                "IDIOT.wav",
+                "080509.wav",
+                "Melodie.wav",
+                "SunsetStrip.wav",
+                "ToadSong.wav",
+                "Wasted.wav"
+        };
+
+        // MusicPlayer 초기화 및 재생 시작
+        MusicPlayer musicPlayer = new MusicPlayer(wavFiles);
+        musicPlayer.playNextSong();
+
+        // 초기 화면을 LoginPanel 설정
+        setContentPane(new LoginPanel(this, musicPlayer));
         setVisible(true);
-
     }
 
-    private class StartPanel extends JPanel {
-
-        public StartPanel() {
-
-            this.setLayout(null);
-
-            JButton check = new JButton("확인");
-            check.setBorderPainted(false); // 테두리를 그리지 않음
-            check.setFocusPainted(false); // 포커스를 얻었을 때 외형을 그리지 않음
-            check.setBounds(670, 420, 100, 55);
-            add(check); // 패널에 추가
-        }
-    }
 }
-
